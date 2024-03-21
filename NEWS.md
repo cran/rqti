c54416a7d96c2aabf25866c04dea7a121f9bc26f
Andrey Shevandrin <shevandrin@gmail.com>
2024-03-21 12:10:02 +0000

    version 0.1.1

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,15 @@
+# rqti 0.1.1
+
+## Bug fixes
+
+* Added the missing qti.js for rendering Rmd-files with tasks.
+
+* Added processing of the pandoc --embed-resources parameter for pandoc versions < 2.19.
+
+## New features
+
+* Identifiers of AssessmentItem, AssessmentSection and AssessmentTest objects are checked for compliance with QTI requirements.
+
 # rqti 0.1.0
 
 * Initial CRAN submission.
--- tests/testthat/test-api_opal.R
+++ tests/testthat/test-api_opal.R
@@ -14,14 +14,14 @@ test_that("Testing auth_opal() and is_logged()", {
     sut3 <- is_logged()
     expect_equal(sut3, TRUE)
 })
-skip()
+# skip()
 test_that("Testing upload to opal and get resources", {
     test_name <- "0fGwTxYRMNVN-test-purposes"
-    file <- test_path("file/rmd/test_rmd_OneInColTable_as_table_T.Rmd")
+    file <- test_path("file/rmd/Gap_primitive.Rmd")
 
     sut1 <- upload2opal(file, display_name = test_name, open_in_browser = FALSE)
     expect_equal(sut1$display_name, test_name)
 
-    sut2 <- get_resources_by_name(display_name = test_name)
-    expect_equal(sut2[[1]]$displayname, test_name)
+    sut2 <- rqti:::get_resources_by_name(display_name = test_name)
+    expect_equal(sut2$displayname, test_name)
 })
